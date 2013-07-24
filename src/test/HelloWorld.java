8940b5d4f02d8319d88eda2913138057ba21b30d
Andrew DeMaria <ademaria@mines.edu>
2013-07-23 20:48:40 -0500

    Added src directory

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * HelloWorld.java
+ * Copyright (C) 2013 Andrew DeMaria (muff1nman) <ademaria@mines.edu>
+ *
+ * All Rights Reserved.
+ */
+
+package test;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
+
+
