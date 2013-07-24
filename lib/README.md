a1944c3251e215c5a57a7610272d8e256e48e7a4
Andrew DeMaria <ademaria@mines.edu>
2013-07-23 21:45:04 -0500

    Added log for java library

---
+++ lib/README.md
@@ -0,0 +1,4 @@
+NEEDED
+---------
+log4j-api-2.0-beta8.jar
+log4j-core-2.0-beta8.jar
--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -7,9 +7,15 @@
 
 package test;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class HelloWorld {
+	private static Logger logger = LogManager.getLogger(HelloWorld.class);
 	public static void main(String[] args) {
+		logger.info("Starting program");
 		System.out.println("Hello World");
+		logger.info("Ending program");
 	}
 }
 
